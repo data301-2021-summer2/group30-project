dd11b12b9c8c0d1f15aaa0d3ea035b47099f55da
Seravin Suteja <seravin@student.ubc.ca>
2021-07-27 01:10:24 -0700

    modified loan and process function

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -12,10 +12,12 @@ def load_and_process(url_or_path_to_csv_file):
 
     df1 = (
         pd.read_csv("C:/Users/serav/Documents/UBC/SUMMER 2021/TERM 2/DATA 301/project-group-30/data/raw/police_killings.csv", encoding = "ISO-8859-1")
-        .drop(['name', 'day', 'year','h_income', 'urate', 'state', 'county_income', 'comp_income','county_bucket', 'nat_bucket', 'share_black', 'share_hispanic', 'college', 'streetaddress', 'pov', 'city', 'latitude', 'longitude', 'state_fp', 'county_fp', 'tract_ce', 'geo_id', 'county_id', 'namelsad', 'lawenforcementagency'], axis =1)
+        .drop(['name', 'day', 'year','h_income', 'state', 'county_income', 'comp_income','county_bucket', 'nat_bucket', 'share_black', 'share_hispanic', 'college', 'streetaddress', 'pov', 'city', 'latitude', 'longitude', 'state_fp', 'county_fp', 'tract_ce', 'geo_id', 'county_id', 'namelsad', 'lawenforcementagency'], axis =1)
         .rename(columns={"p_income": "income"})
         .dropna()
-        .query("age != 'Unknown' & cause != 'Unknown' & armed != 'Unknown' & income != '-'") 
+        .query("age != 'Unknown' & cause != 'Unknown' & armed != 'Unknown' & income != '-'")
+        .replace('White','Caucasian')
+        .replace('Black','African American')
     )
     df1['age'] = pd.to_numeric(df1['age'])
     df1['income'] = pd.to_numeric(df1['income'])
