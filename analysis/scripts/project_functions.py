11bf01156dfa58b5fdc4e52899d0dec158cd08c5
Seravin Suteja <seravin@student.ubc.ca>
2021-07-28 06:03:32 -0700

    added renaming a column in load and process function

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -14,26 +14,27 @@ def load_and_process(url_or_path_to_csv_file):
         pd.read_csv("C:/Users/serav/Documents/UBC/SUMMER 2021/TERM 2/DATA 301/project-group-30/data/raw/police_killings.csv", encoding = "ISO-8859-1")
         .drop(['name', 'day', 'year','h_income', 'state', 'county_income', 'comp_income','county_bucket', 'nat_bucket', 'share_black', 'share_hispanic', 'college', 'streetaddress', 'pov', 'city', 'latitude', 'longitude', 'state_fp', 'county_fp', 'tract_ce', 'geo_id', 'county_id', 'namelsad', 'lawenforcementagency'], axis =1)
         .rename(columns={"p_income": "income"})
+        .rename(columns = {"share_white": "share_Caucasian"})
         .dropna()
         .query("age != 'Unknown' & cause != 'Unknown' & armed != 'Unknown' & income != '-'")
     )
     df1['age'] = pd.to_numeric(df1['age'])
     df1['income'] = pd.to_numeric(df1['income'])
-    df1['share_white'] = pd.to_numeric(df1['share_white'])
+    df1['share_Caucasian'] = pd.to_numeric(df1['share_Caucasian'])
     
     #method chain 2 (Process Data - deal with outliers, create new columns, and replace values)
 
     age_labels = [f"{i} - {i+9}" for i in range(0,80,10)]
     pop_labels = [f"{i} - {i+1000}" for i in range(0,13000,1000)]
     income_labels = [f"{i} - {i+1000}" for i in range(5000,87000,1000)]
-    SW_labels = ['Low', 'Medium', 'Half', 'High', 'Very High']
+    SC_labels = ['Low', 'Medium', 'Half', 'High', 'Very High']
     df2 =(
         df1
         .query("pop < 15000")
         .assign(age_group = lambda x: pd.cut(x['age'], range(0, 90, 10), right=False,labels=age_labels))
         .assign(pop_group = lambda df: pd.cut(df['pop'], range(0, 14000, 1000), right=False, labels=pop_labels))
         .assign(income_bracket = lambda df:pd.cut(df['income'], range(5000,88000,1000), right=False, labels=income_labels))
-        .assign(SW_category = lambda df:pd.qcut(x = df['share_white'], q= 5, precision = 1, labels=SW_labels))
+        .assign(SC_category = lambda df:pd.qcut(x = df['share_Caucasian'], q= 5, precision = 1, labels=SC_labels))
         .reset_index(drop=True) 
         .replace('White','Caucasian')
         .replace('Black','African American')
