2d8b2997acd0bde9e2ca60df6b047eaef6097a56
Seravin Suteja <seravin@student.ubc.ca>
2021-07-28 22:20:28 -0700

    cerated boxplots for finding outliers and updated load nad process function

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -11,12 +11,12 @@ def load_and_process(url_or_path_to_csv_file):
     #method chain 1 (load and Clean data - remove columns and rows, rename columns, deal with missing data, and format data)
 
     df1 = (
-        pd.read_csv("C:/Users/serav/Documents/UBC/SUMMER 2021/TERM 2/DATA 301/project-group-30/data/raw/police_killings.csv", encoding = "ISO-8859-1")
-        .drop(['name', 'day', 'year','h_income', 'state', 'county_income', 'comp_income','county_bucket', 'nat_bucket', 'share_black', 'share_hispanic', 'college', 'streetaddress', 'pov', 'city', 'latitude', 'longitude', 'state_fp', 'county_fp', 'tract_ce', 'geo_id', 'county_id', 'namelsad', 'lawenforcementagency'], axis =1)
+        pd.read_csv(url_or_path_to_csv_file, encoding = "ISO-8859-1")
+        .drop(['name', 'day', 'year','h_income', 'cause', 'state', 'county_income', 'comp_income','county_bucket', 'nat_bucket', 'share_black', 'share_hispanic', 'college', 'streetaddress', 'pov', 'city', 'latitude', 'longitude', 'state_fp', 'county_fp', 'tract_ce', 'geo_id', 'county_id', 'namelsad', 'lawenforcementagency'], axis =1)
         .rename(columns={"p_income": "income"})
         .rename(columns = {"share_white": "share_Caucasian"})
         .dropna()
-        .query("age != 'Unknown' & cause != 'Unknown' & armed != 'Unknown' & income != '-'")
+        .query("age != 'Unknown' & armed != 'Unknown' & income != '-'")
     )
     df1['age'] = pd.to_numeric(df1['age'])
     df1['income'] = pd.to_numeric(df1['income'])
@@ -31,7 +31,7 @@ def load_and_process(url_or_path_to_csv_file):
     df2 =(
         df1
         .query("pop < 15000")
-        .assign(age_group = lambda x: pd.cut(x['age'], range(0, 90, 10), right=False,labels=age_labels))
+        .assign(age_group = lambda df: pd.cut(df['age'], range(0, 90, 10), right=False,labels=age_labels))
         .assign(pop_group = lambda df: pd.cut(df['pop'], range(0, 14000, 1000), right=False, labels=pop_labels))
         .assign(income_bracket = lambda df:pd.cut(df['income'], range(5000,88000,1000), right=False, labels=income_labels))
         .assign(SC_category = lambda df:pd.qcut(x = df['share_Caucasian'], q= 5, precision = 1, labels=SC_labels))
